669aa88085cc39f3aa613b9efeabc5619a54db6a
l-isaro <l.isaro@alustudent.com>
2024-05-17 11:14:18 +0200

    added a few sections to the porto clone

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "web-grey": "#333333",
+        "light-grey": "#d5d5d5",
+        "dark-grey": "#413c3b",
+        "web-brown": "#a18a4d",
+      },
+    },
+  },
+  plugins: [],
+};
